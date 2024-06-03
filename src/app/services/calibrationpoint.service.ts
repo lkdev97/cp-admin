37ba360749b75a8c0072a4b73ffe387c64d0f377
Lars Köhler <lars.koehler@mni.thm.de>
2024-06-03 09:59:55 +0200

    add function to build calibrationpoint and add fingerprint, accesspoint, wifidata to object

--- src/app/services/calibrationpoint.service.ts
+++ src/app/services/calibrationpoint.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CalibrationPoint, Fingerprint, WifiData } from '../models/calibrationpoint';
+import { AccessPoint } from '../models/accesspoint';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalibrationpointService {
 
-  private calibrationPointURL = 'http://localhost:32811/calibrationPoints';
+  private calibrationPointURL = `${environment.calibrationPointService}/calibrationPoints`;
 
   constructor(private _http: HttpClient) { }
 
-  getCalibrationPoints(): Observable<any> {
-    return this._http.get(this.calibrationPointURL);
+  getCalibrationPoints(): Observable<CalibrationPoint> {
+    return this._http.get<CalibrationPoint>(this.calibrationPointURL);
   }
 
   addCalibrationPoint(newCalibrationPoint: any): Observable<any> {
@@ -25,5 +28,39 @@ export class CalibrationpointService {
 
   editCalibrationPoint(calibrationPointID: number, calibrationPointData: any): Observable<any> {
     return this._http.put(`${this.calibrationPointURL}/${calibrationPointID}`, calibrationPointData, { responseType: 'text' })
+  }
+
+  buildCalibrationPoint(lat: number, lng: number, floor: String, building: String, fingerprint: Fingerprint[]): CalibrationPoint {
+    return {
+      lat,
+      lng,
+      floor,
+      building,
+      fingerprints: fingerprint
+    };
+  }
+
+  buildFingerprint(azimuthInDegrees: number, wifiData: WifiData[], accessPoints: AccessPoint[]) {
+    return {
+      azimuthInDegrees,
+      wifiData,
+      accessPoints
+    }
+  }
+
+  addFingerprints(cp: CalibrationPoint, fingerprint: Fingerprint) {
+    cp.fingerprints.push(fingerprint);
+  }
+
+  addWifiData(cp: CalibrationPoint, wifiData: WifiData, azimuthInDegrees: number): void {
+    cp.fingerprints.push({
+      azimuthInDegrees: azimuthInDegrees,
+      wifiData: [wifiData],
+      accessPoints: []
+    });
+  }
+
+  addAccessPoint(cp: CalibrationPoint, accessPoint: AccessPoint, index: number): void {
+    cp.fingerprints[index].accessPoints.push(accessPoint);
   }
 }
